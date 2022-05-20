31bbd4270c49e43a33ee1d48073b5a0ae1874b5c
CLX <73418195+ReverseSacle@users.noreply.github.com>
2022-05-21 01:03:20 +0800

    Update Get_SiteUrl.py

--- Get_SiteUrl.py
+++ Get_SiteUrl.py
@@ -2,14 +2,9 @@ import requests
 import sys
 import time
 
-if (len(sys.argv) >= 2):
-    urls = sys.argv[1].split(',')
-else:
-    #Fill your site which is for organization of comment(填写你的评论管理地址 (后台地址))
-    #https://xxxxx.avosapps.us/
-    urls = "https://www.reversesacle.com/"
+urls = sys.argv[1]
 
-req = requests.get(urls)
-print(f'网址唤醒状态:', 
+req = requests.get(urls[0])
+print(f'网址唤醒状态:',
       time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
      )
