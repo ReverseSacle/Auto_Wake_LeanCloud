6279b770b9bb7fb08817d7c8258e7ae3f47eedb1
CLX <73418195+ReverseSacle@users.noreply.github.com>
2022-05-21 01:11:01 +0800

    Update Get_SiteUrl.py

--- Get_SiteUrl.py
+++ Get_SiteUrl.py
@@ -3,8 +3,10 @@ import sys
 import time
 
 
-urls = sys.argv[1]
+urls = sys.argv[0]
+print(urls)
 req = requests.get(urls[0])
+print(urls[0])
 print(f'网址唤醒状态:',
       time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
      )
