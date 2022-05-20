a528d6e3921558cb450cc4a9a660e4ec83d825d2
CLX <73418195+ReverseSacle@users.noreply.github.com>
2022-05-21 00:55:19 +0800

    Update Get_SiteUrl.py

--- Get_SiteUrl.py
+++ Get_SiteUrl.py
@@ -7,7 +7,7 @@ if (len(sys.argv) >= 2):
 else:
     #Fill your site which is for organization of comment(填写你的评论管理地址 (后台地址))
     #https://xxxxx.avosapps.us/
-    urls = "https://reversesacle.avosapps.us/"
+    urls = "https://www.reversesacle.com/"
 
 req = requests.get(urls)
 print(f'网址唤醒状态:', 
